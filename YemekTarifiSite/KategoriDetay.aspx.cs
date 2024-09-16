71a3168ec92ca96b19b3701e20739774f538728e
Seda Nur Ekici <sedanur.ekc.312@gmail.com>
2024-09-16 15:45:58 +0300

    Kategorilere ait yemekleri listeleme tamamlandı.

--- YemekTarifiSite/KategoriDetay.aspx.cs
+++ YemekTarifiSite/KategoriDetay.aspx.cs
@@ -4,14 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace YemekTarifiSite
 {
     public partial class KategoriDetay : System.Web.UI.Page
     {
+        sqlsinif bgl=new sqlsinif();
+        string kategoriid = "";
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            kategoriid = Request.QueryString["Kategoriid"];
+            SqlCommand komut=new SqlCommand("select * from Tbl_Yemekler Where Kategoriid=@p1",bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1",kategoriid);
+            SqlDataReader dr=komut.ExecuteReader();
+            DataList2.DataSource = dr;
+            DataList2.DataBind();
         }
     }
 }
